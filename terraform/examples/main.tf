be17ca39055aab7fe787659d88ddddddf6ee5161
venuk-1502 <koonamneni1502@gmail.com>
2021-11-08 12:31:36 +0530

    spot requests

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -2,22 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_spot_instance_request" "test" {
+resource "aws_spot_instance_request" "ec2_instance" {
+  count         = length(var.components)
   ami           = "ami-0dc863062bc04e1de"
   instance_type = "t2.micro"
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
-    Name = "my-instance"
+    Name = element(var.components, count.index)
   }
 }
 
 resource "aws_ec2_tag" "ec2_tag" {
-  resource_id = aws_spot_instance_request.test.spot_instance_id
+  count         = length(var.components)
+  resource_id = element(aws_spot_instance_request.ec2_instance.*.spot_instance_id, count.index)
   key         = "Name"
-  value       = "test1"
+  value       = element(var.components, count.index)
 }
 
+variable "components" {
+  default = ["cart", "catalogue", "mongodb"]
+}
 
 resource "aws_security_group" "allow_all" {
   name        = "allow_all_1"
