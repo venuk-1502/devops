93a4da3171a0a8c3afdb3b585a3dd441ca0c2d20
venuk-1502 <koonamneni1502@gmail.com>
2021-11-08 11:48:11 +0530

    spot requests

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -5,8 +5,15 @@ provider "aws" {
 resource "aws_spot_instance_request" "test" {
   ami           = "ami-0dc863062bc04e1de"
   instance_type = "t2.micro"
+  wait_for_fulfillment = true
 
   tags = {
     Name = "my-instance"
   }
+}
+
+resource "aws_ec2_tag" "ec2_tag" {
+  resource_id = aws_spot_instance_request.test.spot_instance_id
+  key         = "Name"
+  value       = "test1"
 }
