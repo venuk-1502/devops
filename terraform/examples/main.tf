cf12166594697e715bdcbd0441a0af2e2c42fd57
venuk-1502 <koonamneni1502@gmail.com>
2021-11-08 12:03:15 +0530

    spot requests

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -6,7 +6,7 @@ resource "aws_spot_instance_request" "test" {
   ami           = "ami-0dc863062bc04e1de"
   instance_type = "t2.micro"
   wait_for_fulfillment = true
-
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
   tags = {
     Name = "my-instance"
   }
@@ -16,4 +16,34 @@ resource "aws_ec2_tag" "ec2_tag" {
   resource_id = aws_spot_instance_request.test.spot_instance_id
   key         = "Name"
   value       = "test1"
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_1"
+  description = "Allow all traffic"
+  vpc_id      = "vpc-0460de79"
+
+  ingress = [
+    {
+      description      = "allow incoming"
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_all_1"
+  }
 }
