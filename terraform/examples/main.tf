dc6d1fd74320c364e85719e350451de409100586
venuk-1502 <koonamneni1502@gmail.com>
2021-11-08 11:11:33 +0530

    spot requests

--- terraform/examples/main.tf
+++ terraform/examples/main.tf
@@ -1,11 +1,12 @@
-resource "aws_instance" "test" {
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_spot_instance_request" "test" {
   ami           = "ami-0dc863062bc04e1de"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "my-instance"
   }
-}
-provider "aws" {
-  region = "us-east-1"
 }
