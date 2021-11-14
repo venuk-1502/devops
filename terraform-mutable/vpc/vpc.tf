21d7df142efe335087c344272c0f2dfde95218af
venuk-1502 <koonamneni1502@gmail.com>
2021-11-14 22:01:29 +0530

    spot requests

--- terraform-mutable/vpc/vpc.tf
+++ terraform-mutable/vpc/vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "vpc" {
 
 }
 
-resource "aws_vpc_ipv4_cidr_block_association" "" {
+resource "aws_vpc_ipv4_cidr_block_association" "vpc_cidr_association" {
   count = length(var.ADD_VPC_CIDR)
   cidr_block = element(var.ADD_VPC_CIDR, count.index)
   vpc_id     = aws_vpc.vpc.id
