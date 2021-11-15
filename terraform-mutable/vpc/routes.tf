1732f2b02f3f21810998d01a9f2f3120388fcb63
venuk-1502 <koonamneni1502@gmail.com>
2021-11-15 11:04:37 +0530

    spot requests

---
+++ terraform-mutable/vpc/routes.tf
@@ -0,0 +1,55 @@
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route  = [
+    {
+      cidr_block                   = var.DEFAULT_VPC_ID
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peering.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
+    }
+  ]
+  tags = {
+    Name = "private_route_table"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route  = [
+    {
+      cidr_block                   = var.VPC_CIDR_MAIN
+      vpc_peering_connection_id    = aws_vpc_peering_connection.peering.id
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
+    }
+  ]
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
+#
+#resource "aws_route" "route" {
+#  route_table_id            = "rtb-4fbb3ac4"
+#  destination_cidr_block    = "10.0.1.0/22"
+#  vpc_peering_connection_id = "pcx-45ff3dc1"
+#  depends_on                = [aws_route_table.testing]
+#}
