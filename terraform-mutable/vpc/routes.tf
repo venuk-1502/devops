9d58e08bb6614c2cc7bdecdcfc89719ccd766625
venuk-1502 <koonamneni1502@gmail.com>
2021-11-15 20:41:52 +0530

    spot requests

--- terraform-mutable/vpc/routes.tf
+++ terraform-mutable/vpc/routes.tf
@@ -4,17 +4,17 @@ resource "aws_route_table" "private_route_table" {
     {
       cidr_block                   = var.DEFAULT_VPC_CIDR
       vpc_peering_connection_id    = aws_vpc_peering_connection.peering.id
-#      carrier_gateway_id           = ""
-#      "destination_prefix_list_id" = ""
-#      "egress_only_gateway_id"     = ""
-#      "gateway_id"                 = ""
-#      "instance_id"                = ""
-#      "ipv6_cidr_block"            = ""
-#      "local_gateway_id"           = ""
-#      "nat_gateway_id"             = ""
-#      "network_interface_id"       = ""
-#      "transit_gateway_id"         = ""
-#      "vpc_endpoint_id"            = ""
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     },
     {
       cidr_block                   = "0.0.0.0/0"
@@ -43,17 +43,17 @@ resource "aws_route_table" "public_route_table" {
     {
       cidr_block                   = var.DEFAULT_VPC_CIDR
       vpc_peering_connection_id    = aws_vpc_peering_connection.peering.id
-#      carrier_gateway_id           = ""
-#      "destination_prefix_list_id" = ""
-#      "egress_only_gateway_id"     = ""
-#      "gateway_id"                 = ""
-#      "instance_id"                = ""
-#      "ipv6_cidr_block"            = ""
-#      "local_gateway_id"           = ""
-#      "nat_gateway_id"             = ""
-#      "network_interface_id"       = ""
-#      "transit_gateway_id"         = ""
-#      "vpc_endpoint_id"            = ""
+      carrier_gateway_id           = ""
+      "destination_prefix_list_id" = ""
+      "egress_only_gateway_id"     = ""
+      "gateway_id"                 = ""
+      "instance_id"                = ""
+      "ipv6_cidr_block"            = ""
+      "local_gateway_id"           = ""
+      "nat_gateway_id"             = ""
+      "network_interface_id"       = ""
+      "transit_gateway_id"         = ""
+      "vpc_endpoint_id"            = ""
     },
     {
       cidr_block                   = "0.0.0.0/0"
