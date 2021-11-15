caf140f4232e2ec2e9b9c7901dadb63ef40a2bfa
venuk-1502 <koonamneni1502@gmail.com>
2021-11-15 10:28:41 +0530

    spot requests

--- terraform-mutable/vpc/subnets.tf
+++ terraform-mutable/vpc/subnets.tf
@@ -1,9 +1,22 @@
-resource "aws_subnet" "private-1" {
+resource "aws_subnet" "private-subnets" {
+  depends_on = [aws_vpc_ipv4_cidr_block_association.vpc_cidr_association]
   count = length(var.PRIVATE_SUBNETS)
   cidr_block = element(var.PRIVATE_SUBNETS, count.index)
   vpc_id     = aws_vpc.vpc.id
   availability_zone = element(var.AZS, count.index)
   tags = {
-    Name = "private-sub-${var.ENV}"
+    Name = "private-sub-${var.ENV}-${count.index}"
   }
 }
+
+resource "aws_subnet" "public-subnets" {
+  depends_on = [aws_vpc_ipv4_cidr_block_association.vpc_cidr_association]
+  count = length(var.PUBLIC_SUBNETS)
+  cidr_block = element(var.PUBLIC_SUBNETS, count.index)
+  vpc_id     = aws_vpc.vpc.id
+  availability_zone = element(var.AZS, count.index)
+  tags = {
+    Name = "public-sub-${var.ENV}-${count.index}"
+  }
+}
+
--- terraform-mutable/vpc/vpc.tf
+++ terraform-mutable/vpc/vpc.tf
@@ -12,3 +12,4 @@ resource "aws_vpc_ipv4_cidr_block_association" "vpc_cidr_association" {
   cidr_block = element(var.ADD_VPC_CIDR, count.index)
   vpc_id     = aws_vpc.vpc.id
 }
+
