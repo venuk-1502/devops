966c024bc868c4593ced36c16547d1205d00599e
venuk-1502 <koonamneni1502@gmail.com>
2021-11-24 15:26:59 +0530

    spot requests

--- terraform-mutable/app-module/lb.tf
+++ terraform-mutable/app-module/lb.tf
@@ -3,6 +3,14 @@ resource "aws_lb_target_group" "tg" {
   port     = var.PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    unhealthy_threshold = 2
+    timeout             = 4
+  }
 }
 
 resource "aws_lb_target_group_attachment" "tg" {
