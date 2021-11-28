bc6cdac501a4fca475bdf713b82d2592fe3bdb0e
venuk-1502 <koonamneni1502@gmail.com>
2021-11-28 15:02:50 +0530

    spot requests

--- jenkins/vars/java.groovy
+++ jenkins/vars/java.groovy
@@ -32,6 +32,9 @@ def call(Map params = [:]) {
             stage('Code Quality') {
                 steps {
                     sh 'echo Code Quality'
+                    sh '''
+                      sonar-scanner -Dsonar.projectKey=${params.COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.21.229:9000 -Dsonar.login=be6cfd1385e726a95a659491011c47f10ca33312
+                    '''
                 }
             }
 
