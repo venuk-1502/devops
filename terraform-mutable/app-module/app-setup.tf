c04ecb17ec251e06dac41991d9cedf85731feb0d
venuk-1502 <koonamneni1502@gmail.com>
2021-12-06 18:45:29 +0530

    spot requests

--- terraform-mutable/app-module/app-setup.tf
+++ terraform-mutable/app-module/app-setup.tf
@@ -2,8 +2,13 @@ locals {
   PRIVATE_IPS = concat(aws_spot_instance_request.spot-instance.*.private_ip, aws_instance.od-instance.*.private_ip)
 }
 
+
 resource "null_resource" "app-setup" {
   count        = length(local.PRIVATE_IPS)
+  triggers = {
+    //private_ip = element(local.PRIVATE_IPS, count.index)
+    abc = timestamp()
+  }
   provisioner "remote-exec" {
     connection {
       host     = element(local.PRIVATE_IPS, count.index)
