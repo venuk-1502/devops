0e4ae9755394e3b0283051c0d644c5868596822c
venuk-1502 <koonamneni1502@gmail.com>
2021-12-06 14:18:30 +0530

    spot requests

--- jenkins/vars/nodejs.groovy
+++ jenkins/vars/nodejs.groovy
@@ -68,9 +68,8 @@ def call(Map params = [:]) {
                     GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \\$NF}'`
                     echo \\${GIT_TAG} >version
                     zip -r ${params.COMPONENT}-\\${GIT_TAG}.zip node_modules server.js version
-                    curl -f -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\\${GIT_TAG}.zip http://172.31.7.184:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\\${GIT_TAG}.zip
+                    curl -f -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\\${GIT_TAG}.zip http://18.208.250.133:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\\${GIT_TAG}.zip
                     '''
-
                 }
             }
 
