1ad93870a050969c28c8b44c65a47b2178fa3a9d
venuk-1502 <koonamneni1502@gmail.com>
2021-12-08 18:39:50 +0530

    spot requests

--- jenkins/vars/python.groovy
+++ jenkins/vars/python.groovy
@@ -64,6 +64,20 @@ def call(Map params = [:]) {
                 }
             }
 
+            stage('App Deployment - Dev Env') {
+                steps {
+                    script {
+                        GIT_TAG = GIT_BRANCH.split('/').last()
+                    }
+                    build job: 'Mutable/App-Deploy', parameters: [
+                            string(name: 'ENV', value: 'dev'),
+                            string(name: 'APP_VERSION', value: "${GIT_TAG}"),
+                            string(name: 'COMPONENT', value: "${params.COMPONENT}")
+                    ]
+                }
+            }
+
+
         }
         post {
             always {
