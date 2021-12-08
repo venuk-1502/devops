393e95891c3be79d9eec18b0673cf336cb708598
venuk-1502 <koonamneni1502@gmail.com>
2021-12-08 18:27:29 +0530

    spot requests

--- jenkins/vars/java.groovy
+++ jenkins/vars/java.groovy
@@ -27,7 +27,7 @@ def call(Map params = [:]) {
             stage('Maven Compile') {
                 steps {
                     sh 'echo Maven Compiler'
-                   // sh 'mvn package'
+                    sh 'mvn package'
                    // sh "echo COMPONENT = ${params.COMPONENT}"
                    // sh 'env'
                     //GIT_BRANCH
