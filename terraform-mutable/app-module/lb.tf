fe1d5c4941766c1cd4cc329841d60e71c811aab7
venuk-1502 <koonamneni1502@gmail.com>
2021-12-09 10:30:24 +0530

    spot requests

--- terraform-mutable/app-module/lb.tf
+++ terraform-mutable/app-module/lb.tf
@@ -37,49 +37,49 @@ resource "aws_lb_listener_rule" "private-listener-rule" {
   }
 }
 
-resource "aws_lb_listener" "publiclb" {
-  count             = var.IS_PRIVATE ? 0 : 1
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-  port              = var.PORT
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
-
-
-#resource "aws_lb_listener" "public-listener-http" {
+#resource "aws_lb_listener" "publiclb" {
 #  count             = var.IS_PRIVATE ? 0 : 1
 #  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-#  port              = "80"
+#  port              = var.PORT
 #  protocol          = "HTTP"
-#
-#  default_action {
-#    type = "redirect"
-#
-#    redirect {
-#      port        = "443"
-#      protocol    = "HTTPS"
-#      status_code = "HTTP_301"
-#    }
-#  }
-#
-#}
-#
-#resource "aws_lb_listener" "public-listener-https" {
-#  count             = var.IS_PRIVATE ? 0 : 1
-#  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-#  port              = "443"
-#  protocol          = "HTTPS"
-#  ssl_policy        = "ELBSecurityPolicy-2016-08"
-#  certificate_arn   = "arn:aws:acm:us-east-1:378784712135:certificate/24f265fc-e213-4323-ad73-66d913193297"
-#
 #
 #  default_action {
 #    type             = "forward"
 #    target_group_arn = aws_lb_target_group.tg.arn
 #  }
-#
 #}
+
+
+resource "aws_lb_listener" "public-listener-http" {
+  count             = var.IS_PRIVATE ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+}
+
+resource "aws_lb_listener" "public-listener-https" {
+  count             = var.IS_PRIVATE ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:378784712135:certificate/24f265fc-e213-4323-ad73-66d913193297"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+}
