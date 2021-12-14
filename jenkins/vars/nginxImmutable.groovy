1fdf234b4345639a7cc89c018d050bc9ad375c21
venuk-1502 <koonamneni1502@gmail.com>
2021-12-14 12:22:25 +0530

    spot requests

--- jenkins/vars/nginxImmutable.groovy
+++ jenkins/vars/nginxImmutable.groovy
@@ -65,7 +65,7 @@ def call(Map params = [:]) {
                     cd  static
                     zip -r ../${params.COMPONENT}-\${GIT_TAG}.zip *
                     cd ..
-                    curl -f -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.16.46:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
+                    curl -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.16.46:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
                     """
                 }
             }
--- jenkins/vars/pythonImmutable.groovy
+++ jenkins/vars/pythonImmutable.groovy
@@ -63,7 +63,7 @@ def call(Map params = [:]) {
                     GIT_TAG=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
                     echo \${GIT_TAG} >version
                     zip -r ${params.COMPONENT}-\${GIT_TAG}.zip *.py requirements.txt ${params.COMPONENT}.ini version
-                    curl -f -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.16.46:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
+                    curl -v -u ${NEXUS} --upload-file ${params.COMPONENT}-\${GIT_TAG}.zip http://172.31.16.46:8081/repository/${params.COMPONENT}/${params.COMPONENT}-\${GIT_TAG}.zip
                     """
                 }
             }
