9a0ed2dbab3d3ba886232f318b0a621238f9b2c8
venuk-1502 <koonamneni1502@gmail.com>
2021-12-28 21:35:43 +0530

    spot requests

--- jenkins/seed.groovy
+++ jenkins/seed.groovy
@@ -192,3 +192,24 @@ pipelineJob('helm-deploy') {
         }
     }
 }
+
+pipelineJob('K8s-DB-Deploy') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/venuk-1502/devops.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('jenkins/Jenkinsfile-all-components')
+            'lightweight'(true)
+        }
+    }
+}
